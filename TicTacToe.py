1ad025318dca3fce604f6d0b804a362c4f76978f
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-04-30 17:17:11 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,137 +1,144 @@
-import numpy as np
 import random
-tboard = np.array([["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]])
-Game = True
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-------------")
-    print("\n")
 
-def player_move(board):
-    inp = int(input("Enter a number from 1 to 9 for X: "))
-    if inp == 1 or inp == 2 or inp == 3:
-        if board[0][inp-1] == "X" or board[0][inp-1] == "O":
-            print("Place already occupied")
-            player_move(board)
-        else:
-            board[0][inp-1] = "X"
-    if inp == 4 or inp == 5 or inp == 6:
-        if board[1][inp-4] == "X" or board[1][inp-4] == "O":
-            print("Place already occupied")
-            player_move(board)
-        else:
-            board[1][inp-4] = "X"
-    if inp == 7 or inp == 8 or inp == 9:
-        if board[2][inp-7] == "X" or board[2][inp-7] == "O":
-            print("Place already occupied")
-            player_move(board)
-        else:
-            board[2][inp-7] = "X"
-    return board
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]
+        self.current_player = "X"
+        self.scores = {"X": 0, "O": 0, "Tie": 0}
+        
+    def print_board(self):
+        print("\n")
+        for i in range(0, 9, 3):
+            print(f" {self.board[i]} | {self.board[i+1]} | {self.board[i+2]} ")
+            if i < 6:
+                print("-----------")
+        print("\n")
+        
+    def make_move(self, position):
+        if self.board[position] == " ":
+            self.board[position] = self.current_player
+            return True
+        return False
+    
+    def check_winner(self, player):
+        # Check rows
+        for i in range(0, 9, 3):
+            if self.board[i] == self.board[i+1] == self.board[i+2] == player:
+                return True
+        
+        # Check columns
+        for i in range(3):
+            if self.board[i] == self.board[i+3] == self.board[i+6] == player:
+                return True
+        
+        # Check diagonals
+        if self.board[0] == self.board[4] == self.board[8] == player:
+            return True
+        if self.board[2] == self.board[4] == self.board[6] == player:
+            return True
+        
+        return False
+    
+    def check_tie(self):
+        return " " not in self.board
+    
+    def ai_move(self):
+        # Simple AI with some intelligence
+        # First check for winning move
+        for i in range(9):
+            if self.board[i] == " ":
+                self.board[i] = "O"
+                if self.check_winner("O"):
+                    return
+                self.board[i] = " "
+        
+        # Then block player's winning move
+        for i in range(9):
+            if self.board[i] == " ":
+                self.board[i] = "X"
+                if self.check_winner("X"):
+                    self.board[i] = "O"
+                    return
+                self.board[i] = " "
+        
+        # Try to take center
+        if self.board[4] == " ":
+            self.board[4] = "O"
+            return
+        
+        # Try to take a corner
+        corners = [0, 2, 6, 8]
+        random.shuffle(corners)
+        for i in corners:
+            if self.board[i] == " ":
+                self.board[i] = "O"
+                return
+        
+        # Take any available edge
+        edges = [1, 3, 5, 7]
+        random.shuffle(edges)
+        for i in edges:
+            if self.board[i] == " ":
+                self.board[i] = "O"
+                return
+    
+    def play(self):
+        print("Welcome to Tic Tac Toe!")
+        print("Enter positions 1-9 as shown below:")
+        print(" 1 | 2 | 3 ")
+        print("-----------")
+        print(" 4 | 5 | 6 ")
+        print("-----------")
+        print(" 7 | 8 | 9 \n")
+        
+        while True:
+            self.board = [" " for _ in range(9)]
+            self.current_player = "X"
+            
+            while True:
+                self.print_board()
+                
+                if self.current_player == "X":
+                    try:
+                        position = int(input("Enter your move (1-9): ")) - 1
+                        if position < 0 or position > 8:
+                            print("Please enter a number between 1 and 9")
+                            continue
+                            
+                        if not self.make_move(position):
+                            print("That position is already taken!")
+                            continue
+                    except ValueError:
+                        print("Please enter a valid number!")
+                        continue
+                else:
+                    print("AI is thinking...")
+                    self.ai_move()
+                
+                if self.check_winner(self.current_player):
+                    self.print_board()
+                    self.scores[self.current_player] += 1
+                    print(f"Player {self.current_player} wins!")
+                    break
+                
+                if self.check_tie():
+                    self.print_board()
+                    self.scores["Tie"] += 1
+                    print("It's a tie!")
+                    break
+                
+                self.current_player = "O" if self.current_player == "X" else "X"
+            
+            print("\nScores:")
+            print(f"Player X: {self.scores['X']}")
+            print(f"Player O: {self.scores['O']}")
+            print(f"Ties: {self.scores['Tie']}")
+            
+            play_again = input("\nPlay again? (y/n): ").lower()
+            if play_again != 'y':
+                print("Thanks for playing!")
+                break
 
-def computer_move(board):
-    # input
-    inp = random.randint(1, 9) 
-    if inp == 1 or inp == 2 or inp == 3:
-        if board[0][inp - 1] == "X" or board[0][inp - 1] == "O":
-           
-            computer_move(board)
-        else:
-            board[0][inp - 1] = "O"
-    if inp == 4 or inp == 5 or inp == 6:
-        if board[1][inp - 4] == "X" or board[1][inp - 4] == "O":
-         
-            computer_move(board)
-        else:
-            board[1][inp - 4] = "O"
-    if inp == 7 or inp == 8 or inp == 9:
-        if board[2][inp - 7] == "X" or board[2][inp - 7] == "O":
-         
-            computer_move(board)
-        else:
-            board[2][inp - 7] = "O"
-    return board
-
-
-def check_win_horizontal(board):
-    if (board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X") or (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O"):
-        if board[0][0] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    if (board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X") or (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O"):
-        if board[1][0] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    if (board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X") or (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O"):
-        if board[2][0] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    else:
-        return True
-
-
-def check_win_vertical(board):
-    if (board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X") or (board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O"):
-        if board[0][0] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    if (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X") or (board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O"):
-        if board[0][1] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    if (board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X") or (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O"):
-        if board[0][2] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    else:
-        return True
-
-def check_win_diagonal(board):
-    if (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X") or (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O"):
-        if board[0][0] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    if (board[2][0] == "X" and board[1][1] == "X" and board[0][2] == "X") or (board[2][0] == "O" and board[1][1] == "O" and board[0][2] == "O"):
-        if board[0][2] == "X":
-            print("Player X won")
-        else:
-            print("Player O won")
-        return False
-    else:
-        return True
-
-def check_tie(board):
-    if "-" not in board:
-        print("It's a tie")
-        return False
-    else:
-        return True
-
-if __name__ == '__main__':
-    print_board(tboard)
-    while Game:
-        tboard = player_move(tboard)
-        print_board(tboard)
-        if not check_win_horizontal(tboard) or not check_win_vertical(tboard) or not check_win_diagonal(tboard) or not check_tie(tboard):
-            break
-        print("Computers turn =>")
-       
-        tboard = computer_move(tboard)
-        print_board(tboard)
-        if not check_win_horizontal(tboard) or not check_win_vertical(tboard) or not check_win_diagonal(tboard) or not check_tie(tboard):
-            break
+# Start the game
+game = TicTacToe()
+game.play()
